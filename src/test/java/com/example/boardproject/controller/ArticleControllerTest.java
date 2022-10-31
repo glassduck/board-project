096a12e4610763f560796fe9b20023f839e339af
sangwooPark <whdrnrdk@gmail.com>
2022-10-31 22:11:52 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드를 작성하여 테스트를 통과하게 열어주고
    적당한 뷰의 골격을 생성. 
    테스트에서 mediaType에 encdoing utf-8도 검사가능하도록 compatible 옵션으로 수정.

--- src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
@@ -25,7 +25,7 @@ class ArticleControllerTest {
         this.mockMvc = mockMvc;
     }
 
-    @Disabled("구현 중")
+//    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -34,7 +34,7 @@ class ArticleControllerTest {
         // When & Then
         mockMvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
